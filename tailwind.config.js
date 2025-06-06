2db7d7d6f3ca8c2ffbf1eff6c3b6e1fde425a0fb
Mykola Korzh <114113283+mykolakorzh@users.noreply.github.com>
2025-06-07 01:50:48 +0200

    fixed all the hell with custom shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {
-      boxShadow: {
-       'skeuomorphic': [
-          'inset 0 -1.5px 1px rgba(74, 50, 40, 0.1)', // First inner shadow
-          'inset 0 1.5px 1px rgba(255, 255, 255, 0.75)',   // Second inner shadow
-          '0 0 0 1px rgba(74, 50, 40, 0.07)',            // Drop shadow
-        ].join(', ')
-
-    },
-  },
+    extend: {},
   plugins: [],
 }
 }
